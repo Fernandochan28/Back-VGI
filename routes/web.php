ed2781b7082911b0640b814984cfbc09bc732861
Eddyel <eddyel.yam@gmail.com>
2024-11-09 12:36:18 -0600

    Configuracion api

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PacienteController;
-use App\Http\Controllers\DoctorController;
-use App\Http\Controllers\HistorialPacienteController;
-use App\Http\Controllers\TestController;
-use App\Http\Controllers\ResultadoTestController;
-use app\Http\Controllers\UserController;
-use App\Http\Middleware\CorsMiddleware;
-
+use App\Http\Controllers\UserController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 Route::get('/', function () {
     return view('welcome');
-});
-
-Route::resource('pacientes', PacienteController::class);
-Route::resource('doctores', DoctorController::class);
-Route::resource('historiales', HistorialPacienteController::class);
-Route::resource('tests', TestController::class);
-Route::resource('resultados', ResultadoTestController::class);
-Route::post('/users', [UserController::class, 'store']);
-Route::post('/pacientes', [PacienteController::class, 'store']);
-
-Route::middleware([CorsMiddleware::class])->group(function () {
-    Route::get('/user', [UserController::class, 'index']);
-    // Otras rutas que necesiten CORS
 });
